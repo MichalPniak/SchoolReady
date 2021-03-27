53855ae4d5f2983c85716a2c04d204c4c1426f44
Michał Pniak <pniak.michal@gmail.com>
2021-03-27 16:23:59 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = require('./routes/index');
+
+// Set up express app
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/', router);
+
+module.exports = app;
+
